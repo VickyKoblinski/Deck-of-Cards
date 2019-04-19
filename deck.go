4ed97c9fb372f78b7420988e9fc22c227478e4d0
Kevin Luu <kluu@epictech.cm>
2019-04-19 11:05:03 -0400

    added saveToFile and newDeckFromFile function

--- deck.go
+++ deck.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
 	"strings"
 )
 
+// create 'deck' which is a slice of strings
 type deck []string
 
+// function to return a new deck
 func newDeck() deck {
 	cards := deck{}
 	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
@@ -33,7 +37,25 @@ func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
 }
 
-// converts deck into a single string
+// converts a deck into a single string
 func (d deck) toString() string {
 	return strings.Join([]string(d), ",")
+}
+
+// saves a deck to a file
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+// returns a deck from a file
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+	// splits the byte into slice
+	s := strings.Split(string(bs), ",")
+	// convert to type of deck and return
+	return deck(s)
 }
